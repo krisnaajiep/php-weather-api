ffc94a814b1b23da5346bd60cd1ca1506de3e21e
Krisna Ajie Prasetyo <krisnaajiep@gmail.com>
2025-01-02 19:13:49 +0700

    feat(deps): load `.env` in `index.php` file

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
 require_once 'functions.php';
 
 echo weather($_GET['location']);
